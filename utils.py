d3b8e5d266d754585c56273287a9b1cd4b40c5a6
debrevitatevitae <giotb92@gmail.com>
2022-12-31 20:07:22 +0100

    utils.py: fix parameter updated and add printout

--- utils.py
+++ utils.py
@@ -14,12 +14,14 @@ def sse(preds:np.ndarray, labels:np.ndarray) -> float:
     """
     return np.sum(np.inner(preds-labels, preds-labels))
 
-def gd_optimise(params0:np.ndarray, fun:Callable, grad_fun:Callable, eta:float=1e-2, tol:float=1e-5, max_iter:int=1000) -> Tuple[np.ndarray, List[float]]:
+def gd_optimise(params0:np.ndarray, fun:Callable, grad_fun:Callable, eta:float=1e-2, tol:float=1e-4, max_iter:int=10_000) -> Tuple[np.ndarray, List[float]]:
     fun_hist = []
     params_old = params0
     for it in range(max_iter):
         params_new = params_old - eta*grad_fun(params_old)
         fun_hist.append(fun(params_new))
-        if np.abs(params_new - params_old) < tol:
+        if np.all(np.abs(params_new - params_old)) < tol:
             break
+        params_old = params_new
+    print(f"Optimisation completed. Iterations = {it+1:d}. Final loss value = {fun_hist[-1]:4f}")
     return params_new, fun_hist
