64067f0c6d06d87344059ade47fd5a04b80e00f5
debrevitatevitae <giotb92@gmail.com>
2022-12-31 16:52:19 +0100

    gd-example.py: add data generation

--- gd-example.py
+++ gd-example.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Tuple
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -9,10 +10,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def my_sum(a:float, b:float) -> float:
-	return a + b
+def generator(x:float, params:np.ndarray=np.array([3., 2., .7, .5])) -> float:
+	return params[0]*np.cos(params[1]*x + params[2]) + params[3]
+
+def generate_data(n:int, noise_mag:float=0.01, params:np.ndarray=np.array([3., 2., .7, .5])) -> Tuple[np.ndarray, np.ndarray]:
+	X = np.random.uniform(-np.pi/2, np.pi/2, size=(n,))
+	y = generator(X, params=params) + noise_mag*np.random.randn(n)
+	return X, y
 
 
 if __name__ == '__main__':
 	np.random.seed(0)
-	print(my_sum(3, 5))
+	
+	#%% Generate data and plot the generator and the data
+	X_grid = np.linspace(-np.pi/2, np.pi/2, 200)
+	X, y = generate_data(20, noise_mag=.1)
+
+	fig, ax = plt.subplots()
+	ax.plot(X_grid, generator(X_grid), 'b-', label='generator')
+	ax.scatter(X, y, marker='o', facecolor='k', edgecolor='k', alpha=.7, label='data')
+	ax.set(xlabel='x', ylabel='y', title='Generator and data for regression.')
+	ax.grid()
+	ax.legend()
+	plt.show()
